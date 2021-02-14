ce6ecdd677cffc071c0443374c1849ae42540267
Joshua Hoeflich <josh@joshuahoeflich.com>
2021-02-14 08:27:47 -0600

    Renames functions and macros

--- src/repl-test/lib.lisp
+++ src/repl-test/lib.lisp
@@ -1,35 +1,35 @@
 (defpackage #:repl-test
   (:use #:common-lisp)
-  (:export #:test #:xtest #:check #:xcheck))
+  (:export #:xtest #:test))
 
-(defmacro xtest (exp)
+(defmacro xtest-values (exp)
   (declare (ignore exp))
   `:ignore)
 
-(defmacro test (exp)
+(defmacro test-values (exp)
   `(handler-case (values (if ,exp :pass :fail) ',exp nil)
      (t (err) (values :error ',exp err))))
 
-(defun handle-test-result (result s-exp err)
+(defun handle-test-values (result s-exp err)
   (cond
     ((not (null err)) (format t "Form ~a errored: ~a~%" s-exp err))
-    ((eq result :fail) (format t "Test failed: ~a~%" s-exp)))
+    ((eq result :fail) (format t "Test-Values failed: ~a~%" s-exp)))
   result)
 
-(defmacro xcheck (exp &key (handler 'handle-test-result))
+(defmacro xtest (exp &key (handler 'handle-test-values))
   (declare (ignore exp handler))
   `:ignore)
 
-(defmacro check (exp &key (handler 'handle-test-result))
-  `(multiple-value-bind (result s-exp err) (test ,exp)
+(defmacro test (exp &key (handler 'handle-test-values))
+  `(multiple-value-bind (result s-exp err) (test-values ,exp)
      (,handler result s-exp (or err nil))))
 
 ;; TODO: Make this code:
 ;; (defsuite this-suite ((:before setup-fn) (:after teardown-fn))
-;;   (check (= 3 4))
-;;   (check (= 2 3))
+;;   (test (= 3 4))
+;;   (test (= 2 3))
 ;;   (format t "This is a side effect!~%")
-;;   (xcheck (= 2 3)))
+;;   (xtest (= 2 3)))
 ;; Build a giant thunk which executes its body in order and agregates
 ;; its results into a struct. Then make this function:
 ;; (run-suites this-suite)
